1063b73c2a1e0ba3d5a3a0d1429cfa186d3181cd
divyansh1705 <divyanshdubey1713@gmail.com>
2025-01-31 19:22:23 +0530

    Changes for deployment

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,11 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./src/lib/db.js";
 import cors from "cors"
 import { app,server } from "./src/lib/socket.js";
+import path from "path";
 
 dotenv.config();
 const PORT=process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({limit:"20mb"}));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
@@ -24,6 +26,14 @@ app.use(
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 server.listen(PORT,()=>{
     console.log(`Server is listenong on port ${PORT}`);
